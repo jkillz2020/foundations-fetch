957c77a0df42a3e5002a15f4682f66468aaf1cdc
jkillz2020 <kilgore.josh@gmail.com>
2017-01-10 20:51:22 -0600

    add http request

---
+++ app.js
@@ -0,0 +1,11 @@
+;(function(){
+  const app = angular.module('fetchApp', [])
+  app.controller('MainController', function(requestService){
+
+  });
+
+  app.service('requestService', function($http){
+    this.get = (url) => $http.get(url);
+  });
+})();
+
