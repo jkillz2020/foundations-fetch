9bc61d96729f85971cc2a939bf647b0f56f10632
jkillz2020 <kilgore.josh@gmail.com>
2017-01-12 11:47:59 -0600

    select dropdown working

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,15 +2,47 @@
     angular.module("fetchApp").controller("MainController", function ($scope, requestService, Response){
         $scope.responses = [];
         $scope.url = "http://httpstat.us/200";
-
         $scope.fetch = () => {
-            requestService.get($scope.url).then((success) => {
+          if ($scope.testicles == 'GET') {
+            console.log('scope dot balls', $scope.testicles)
+              requestService.get($scope.url).then((success) => {
                 const response = new Response(success);
                 $scope.responses.push(response);
+                console.log($scope.testicles)
             }, (error) => {
                 // debugger
                 //something else
             })
+          } else if ($scope.testicles == 'POST'){
+            requestService.post($scope.url).then((success) => {
+                const response = new Response(success);
+                $scope.responses.push(response);
+                console.log($scope.testicles)
+            }, (error) => {
+                // debugger
+                //something else
+            })
+          } else if ($scope.testicles == 'OPTIONS') {
+            // requestService.options($scope.url).then((success) => {
+            //     const response = new Response(success);
+            //     $scope.responses.push(response);
+             //     console.log($scope.testicles)
+            // // }, (error) => {
+            // //     debugger
+            // //     something else
+            // })
+          } else if ($scope.testicles = 'HEAD') {
+            requestService.head($scope.url).then((success) => {
+                const response = new Response(success);
+                $scope.responses.push(response);
+                console.log($scope.testicles)
+            }, (error) => {
+                // debugger
+                //something else
+            })
+          } else {
+            console.log('danger, will robinson');
+          }
         };
     });
 })();
